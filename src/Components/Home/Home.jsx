55e6e6907fce10b53f0e1b1f507a8946b16ade44
Shad Azam <azamshad08@gmail.com>
2023-07-13 21:36:14 +0530

    update

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 import "./home.css";
-import video from '../../Assets/splash-1440p.mp4'
-
+import video from "../../Assets/splash-1440p.mp4";
+import { GrLocation } from "react-icons/gr";
+import { HiFilter } from "react-icons/hi";
+import { FiFacebook } from "react-icons/fi";
+import { AiOutlineInstagram } from "react-icons/ai";
+import { SiTripadvisor } from "react-icons/si";
+import { BsListTask } from "react-icons/bs";
+import { TbApps } from "react-icons/tb";
 
 const Home = () => {
   return (
@@ -11,12 +17,48 @@ const Home = () => {
 
       <div className="homeContent container">
         <div className="textDiv">
-          <span className="smallText">
-            Our Package
-          </span>
-          <h1 className="homeTitle">
-            Search Your Holiday
-          </h1>
+          <span className="smallText">Our Package</span>
+          <h1 className="homeTitle">Search Your Holiday</h1>
+        </div>
+
+        <div className="cardDiv grid">
+          <div className="destinationInput">
+            <label htmlFor="city">Search Your destination:</label>
+            <div className="input flex">
+              <input type="text" placeholder="Enter name here...." />
+              <GrLocation className="icon" />
+            </div>
+          </div>
+          <div className="dateInput">
+            <label htmlFor="date">Select your date:</label>
+            <div className="input flex">
+              <input type="date" />
+            </div>
+          </div>
+          <div className="priceInput">
+            <div className="label_total flex">
+              <label htmlFor="price">Max price:</label>
+              <h3 className="total">$5000</h3>
+            </div>
+            <div className="input flex">
+              <input type="range" max="5000" min="1000" />
+            </div>
+          </div>
+          <div className="searchOptions flex">
+            <HiFilter className="icon" />
+            <span>MORE FILTERS</span>
+          </div>
+        </div>
+        <div className="homeFooterIcons flex">
+          <div className="rightIcons">
+            <FiFacebook className="icon" />
+            <AiOutlineInstagram className="icon" />
+            <SiTripadvisor className="icon" />
+          </div>
+          <div className="leftIcons">
+            <BsListTask className="icon" />
+            <TbApps className="icon" />
+          </div>
         </div>
       </div>
     </section>
