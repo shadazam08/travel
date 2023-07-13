48f6fe7a9ba95448d2d1e608bf99bba4903c1399
Shad Azam <azamshad08@gmail.com>
2023-07-13 22:32:30 +0530

    update

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,8 +1,68 @@
 import React from "react";
 import "./main.css";
+import { HiOutlineLocationMarker } from "react-icons/hi";
+import { HiOutlineClipboardCheck } from "react-icons/hi";
+import img from "../../Assets/japan.jpg";
+
+const Data = [
+  {
+    id: 1,
+    imgSrc: img,
+    destTitle: "Bora Bora",
+    location: "New Zealand",
+    grade: "CULTURAL RELAX",
+    fees: "$700",
+    description:
+      "The epitome of romance, Bora Bora is one of the best travel destinations in the world. This Place is Known for its luxurious stays and adventurous activities. ",
+  },
+];
 
 const Main = () => {
-  return <div>Main</div>;
+  return (
+    <section className="main container section">
+      <div className="secTitle">
+        <h3 className="title">Most visited destinations</h3>
+      </div>
+      <div className="secContent grid">
+        {/* we are use high oder array method */}
+        {Data.map(
+          ({ id, imgSrc, destTitle, location, grade, fees, description }) => {
+            return (
+              <div key={id} className="singleDestination">
+                <div className="imageDiv">
+                  <img src={imgSrc} alt={destTitle} />
+                </div>
+                <div className="cardInfo">
+                  <h4 className="destTitle">{destTitle}</h4>
+                  <span className="continent flex">
+                    <HiOutlineLocationMarker className="icon" />
+                    <span className="name">{location}</span>
+                  </span>
+                  <div className="fees flex">
+                    <div className="grade">
+                      <span>
+                        {grade}
+                        <small>+1</small>
+                      </span>
+                    </div>
+                    <div className="price">
+                      <h5>{fees}</h5>
+                    </div>
+                  </div>
+                  <div className="desc">
+                    <p>{description}</p>
+                  </div>
+                  <button className="btn flex">
+                    DETAILS <HiOutlineClipboardCheck className="icon" />
+                  </button>
+                </div>
+              </div>
+            );
+          }
+        )}
+      </div>
+    </section>
+  );
 };
 
 export default Main;
